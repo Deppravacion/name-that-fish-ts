db9fa8e6d90ddc2435931a75f6ebef6ccf75b7ea
Tyson <deppravacion@gmail.com>
2023-07-09 19:44:26 -0600

    scoreBoard functional

--- src/Components/Functional/FunctionalScoreBoard.tsx
+++ src/Components/Functional/FunctionalScoreBoard.tsx
@@ -1,17 +1,13 @@
 import "./styles/score-board.css";
 import { TComponentProps } from "./FunctionalApp";
-//  Where the score is presented
-
 
 export const  FunctionalScoreBoard: React.FunctionComponent<TComponentProps> =  ({fishState: {correctScore, incorrectScore}}:TComponentProps) => {
-console.log(correctScore);
-
   const answersLeft = ["trout", "salmon", "tuna", "shark"];
   return (
     <div id="score-board">
       <div>Incorrect 🔻: {incorrectScore}</div>
       <div id="choices-left">
-        {answersLeft.map((answer) => (
+        {answersLeft.slice((correctScore + incorrectScore), answersLeft.length).map((answer) => (
           <div key={answer} className="choice">
             {answer}
           </div>
